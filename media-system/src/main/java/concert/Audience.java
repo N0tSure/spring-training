4051d85016b04fd458b80ffaa134a2ee85a9773b
NotSure <ASir2089@gmail.com>
2017-04-10 23:13:40 +0300

    * aspect files set as components  * enabled AspectJ aspect annotation

--- media-system/src/main/java/concert/Audience.java
+++ media-system/src/main/java/concert/Audience.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * Created on 10.04.2017.
@@ -13,25 +14,26 @@ import org.slf4j.LoggerFactory;
  * @author Artemis A. Sirosh
  */
 @Aspect
+@Component
 public class Audience {
     private static final Logger LOGGER = LoggerFactory.getLogger(Audience.class);
 
-    @Before("execution(* concert.Performance.perform(..))")
+    @Before("execution(** concert.Performance.perform(..))")
     public void silencePhones() {
         LOGGER.info("Silencing phones");
     }
 
-    @Before("execution(* concert.Performance.perform(..))")
+    @Before("execution(** concert.Performance.perform(..))")
     public void takeSeats() {
         LOGGER.info("Taking seats");
     }
 
-    @AfterReturning("execution(* concert.Performance.perform(..))")
+    @AfterReturning("execution(** concert.Performance.perform(..))")
     public void applause() {
         LOGGER.info("CLAP CLAP CLAP!!!");
     }
 
-    @AfterThrowing("execution(* concert.Performance.perform(..))")
+    @AfterThrowing("execution(** concert.Performance.perform(..))")
     public void demandRefund() {
         LOGGER.info("Demanding refund");
     }
--- media-system/src/main/java/concert/WoodStockFestival.java
+++ media-system/src/main/java/concert/WoodStockFestival.java
@@ -2,12 +2,14 @@ package concert;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * Created on 10.04.2017.
  *
  * @author Artemis A. Sirosh
  */
+@Component
 public class WoodStockFestival implements Performance {
     private static final Logger LOGGER = LoggerFactory.getLogger(WoodStockFestival.class);
 
