61432ea24dddc608983033056453c5447f7ca4ce
N0tSure <ASir2089@gmail.com>
2017-04-10 13:01:34 +0300

    Simple aspect added

---
+++ media-system/src/main/java/concert/Audience.java
@@ -0,0 +1,38 @@
+package concert;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created on 10.04.2017.
+ * A performance isn’t a performance without an audience.
+ * @author Artemis A. Sirosh
+ */
+@Aspect
+public class Audience {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Audience.class);
+
+    @Before("execution(* concert.Performance.perform(..))")
+    public void silencePhones() {
+        LOGGER.info("Silencing phones");
+    }
+
+    @Before("execution(* concert.Performance.perform(..))")
+    public void takeSeats() {
+        LOGGER.info("Taking seats");
+    }
+
+    @AfterReturning("execution(* concert.Performance.perform(..))")
+    public void applause() {
+        LOGGER.info("CLAP CLAP CLAP!!!");
+    }
+
+    @AfterThrowing("execution(* concert.Performance.perform(..))")
+    public void demandRefund() {
+        LOGGER.info("Demanding refund");
+    }
+}
