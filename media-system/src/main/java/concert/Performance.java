379e0066ebc5a774e642182e769b0e3d274904f5
N0tSure <ASir2089@gmail.com>
2017-04-10 13:01:00 +0300

    Created performance interface and simple realization

---
+++ media-system/src/main/java/concert/Performance.java
@@ -0,0 +1,11 @@
+package concert;
+
+/**
+ * Created on 10.04.2017.
+ * Performance represents any kind of live performance,
+ * such as a stage play, a movie,or a concert.
+ * @author Artemis A. Sirosh
+ */
+public interface Performance {
+    void perform();
+}
