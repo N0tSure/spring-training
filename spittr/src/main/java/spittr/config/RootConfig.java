658e87b34cbd9650a865effdfe60f55b2291f8e7
Artyom Sirosh <ASir2089@gmail.com>
2017-04-22 23:05:21 +0300

    * configurations class created

---
+++ spittr/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,9 @@
+package spittr.config;
+
+/**
+ * Created on 22 Apr, 2017.
+ *
+ * @author Artemis A. Sirosh
+ */
+public class RootConfig {
+}
---
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,15 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created on 22 Apr, 2017.
+ *
+ * @author Artemis A. Sirosh
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
