83b4f9d66e29e7dc1e7447385b1f10e6d82aa8e9
N0tSure <ASir2089@gmail.com>
2017-04-28 14:31:15 +0300

    * controllers for spitter's pages tests written and were implemented

--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -1,8 +1,11 @@
 package spittr.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import spittr.data.SpitterRepository;
+import spittr.model.Spitter;
 
 /**
  * Created on 28.04.2017.
@@ -13,9 +16,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/spitter")
 public class SpitterController {
 
+    private final SpitterRepository repository;
+
+    @Autowired
+    public SpitterController(SpitterRepository spitterRepository) {
+        this.repository = spitterRepository;
+    }
+
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String showRegistrationForm() {
         return "registerForm";
+    }
+
+    public String processingRegistration(Spitter spitter) {
+
+        repository.save(spitter);
+        return "redirect:/spitter/" + spitter.getUsername();
     }
 
 }
--- spittr/src/test/java/spittr/web/SpitterControllerTest.java
+++ spittr/src/test/java/spittr/web/SpitterControllerTest.java
@@ -1,10 +1,18 @@
 package spittr.web;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import spittr.data.SpitterRepository;
+import spittr.model.Spitter;
+
+import java.lang.reflect.Field;
+
 
 /**
  * Created on 28.04.2017.
@@ -12,10 +20,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  * @author Artemis A. Sirosh
  */
 public class SpitterControllerTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpitterControllerTest.class);
 
     @Test
     public void fail_if_not_show_registration_form() throws Exception {
-        SpitterController controller = new SpitterController();
+        SpitterController controller = new SpitterController(null);
 
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
         mockMvc.perform(MockMvcRequestBuilders.get("/spitter/register"))
@@ -23,4 +32,34 @@ public class SpitterControllerTest {
 
     }
 
+    @Test
+    public void shouldProcessRegistration() throws Exception {
+
+        SpitterRepository mockRepository = Mockito.mock(SpitterRepository.class);
+
+        Spitter unsaved =
+                new Spitter("sirosh", "asirosh", "Artemis", "Sirosh");
+
+        Spitter saved = new Spitter("sirosh", "asirosh", "Artemis", "Sirosh");
+        Field idField = Spitter.class.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(saved, 42L);
+
+        LOGGER.info("Unsaved: id = {}, spitter = {}", unsaved.getId(), unsaved);
+        LOGGER.info("Saved: id = {}, spitter = {}", saved.getId(), saved);
+
+        Mockito.when(mockRepository.save(unsaved)).thenReturn(saved);
+
+        SpitterController controller = new SpitterController(mockRepository);
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/spitter/register")
+                .param("firstName", "Artemis")
+                .param("lastName", "Sirosh")
+                .param("username", "sirosh")
+                .param("password", "asirosh")
+        ).andExpect(MockMvcResultMatchers.redirectedUrl("/spitter/sirosh"));
+
+        Mockito.verify(mockRepository, Mockito.atLeastOnce()).save(unsaved);
+    }
 }
