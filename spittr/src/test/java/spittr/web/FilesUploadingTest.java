f798a6805b5edc0e8a9740c4a92fe641bc98cd7c
N0tSure <ASir2089@gmail.com>
2017-11-08 20:57:22 +0300

    * test for uploading image updated

--- spittr/src/test/java/spittr/web/FilesUploadingTest.java
+++ spittr/src/test/java/spittr/web/FilesUploadingTest.java
@@ -1,8 +1,14 @@
 package spittr.web;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
@@ -18,6 +24,8 @@ import org.springframework.web.context.WebApplicationContext;
 import spittr.config.RootConfig;
 import spittr.config.WebConfig;
 
+import java.io.File;
+
 /**
  * <p>
  * Created on 02.11.2017.
@@ -29,11 +37,41 @@ import spittr.config.WebConfig;
 @WebAppConfiguration("file:web")
 @ContextConfiguration(classes = {RootConfig.class, WebConfig.class}, loader = AnnotationConfigWebContextLoader.class)
 public class FilesUploadingTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FilesUploadingTest.class);
+
+    @ClassRule
+    public static EnvironmentVariables environmentVariables = new EnvironmentVariables();
+
+    @ClassRule
+    public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private static File profilePictureLocation;
+
+    private static File temporalProfilePictureLocation;
 
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @BeforeClass
+    public static void environmentSetUp() throws Exception {
+        profilePictureLocation = temporaryFolder.newFolder();
+        temporalProfilePictureLocation = temporaryFolder.newFolder();
+
+        LOGGER.info("Picture location: {}", profilePictureLocation.getAbsolutePath());
+        LOGGER.info("Picture temporal location: {}", temporalProfilePictureLocation.getAbsolutePath());
+
+        environmentVariables.set(
+                "spittr.resources.pictures.profile",
+                profilePictureLocation.getAbsolutePath()
+        );
+
+        environmentVariables.set(
+                "spittr.tmp.resources.pictures.profile",
+                temporalProfilePictureLocation.getAbsolutePath()
+        );
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -50,7 +88,15 @@ public class FilesUploadingTest {
                 MediaType.MULTIPART_FORM_DATA_VALUE,
                 "foo".getBytes());
 
-        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/spitter/register").file(profilePictureFile))
+        mockMvc.perform(
+                MockMvcRequestBuilders.fileUpload("/spitter/register")
+                        .file(profilePictureFile)
+                        .param("firstName", "Artemis")
+                        .param("lastName", "Sirosh")
+                        .param("email", "sirosh@iac.spb.ru")
+                        .param("username", "sirosh")
+                        .param("password", "asirosh")
+        )
                 .andDo(MockMvcResultHandlers.log());
 
     }
