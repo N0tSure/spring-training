a2dca85029d534e993e4f7e121fd560f7de98b7d
N0tSure <ASir2089@gmail.com>
2017-11-08 20:56:49 +0300

    * imports optimized

--- spittr/src/main/java/spittr/web/SpitterController.java
+++ spittr/src/main/java/spittr/web/SpitterController.java
@@ -1,7 +1,6 @@
 package spittr.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,8 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 import spittr.data.SpitterRepository;
 import spittr.model.Spitter;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 /**
